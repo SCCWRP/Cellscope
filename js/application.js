4f935652d15e6bc49a066c0d0aa4bb0e93aaac3a
Paul Smith <pauls@sccwrp.org>
2015-02-25 12:51:12 -0800

    cleanup10

--- js/application.js
+++ js/application.js
@@ -12,7 +12,6 @@ var appRouter = new (Backbone.Router.extend({
 	     var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
 	    if(deviceType == "iPhone"){
 			$('.ui-title').css('font-size','2em');
-			$('.ui-radio').css('width','100px');
 			$('#multi-view .ui-btn-text').css('font-size','18px');
 			$('#multi-view').css('margin-left','15%');
 			$('#multi-select').css('margin-left','5%');
