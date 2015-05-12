df85935d07f46915bfaa75bd712a12fde2fecdce
Paul Smith <pauls@sccwrp.org>
2015-05-12 07:18:04 -0700

    add multiple image picker library15

--- js/views/IntroView.js
+++ js/views/IntroView.js
@@ -85,6 +85,7 @@ var IntroView = Backbone.View.extend({
 					alert("entries.length: "+(entries.length - 1));
 					if(i == (entries.length - 1)){
 						lastentry = true;
+						alert("lastentry");
 					}
 					if(entry.isFile){
 						app.uploadFile(filesystem,entry,lastentry);
